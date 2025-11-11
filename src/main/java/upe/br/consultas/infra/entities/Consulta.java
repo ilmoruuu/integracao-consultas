655ef395553a97d473711c46cd2af8eba81611aa
Murilo Lucena <muriloglucenacanal@gmail.com>
2025-11-11 09:32:16 -0300

    feat: adição da entidade Consulta

---
+++ src/main/java/upe/br/consultas/infra/entities/Consulta.java
@@ -0,0 +1,27 @@
+package upe.br.consultas.infra.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Consulta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    private LocalDate data;
+    private String descricao;
+    //Adicionar uma entidade de Médico
+    //Adicionar uma entidade de Recepcionista
+    //Adicionar uma entidade de paciente
+}
