eee14f92435120666c67980fe9a28db0910fc7db
Murilo Lucena <muriloglucenacanal@gmail.com>
2025-11-27 16:37:04 -0300

    feat: adição do DTO e do Service de Paciente

--- src/main/java/upe/br/consultas/controller/DTO/paciente/PacienteDTO.java
+++ src/main/java/upe/br/consultas/controller/DTO/paciente/PacienteDTO.java
@@ -1,4 +1,30 @@
 package upe.br.consultas.controller.DTO.paciente;
 
-public record PacienteDTO() {
+import upe.br.consultas.infra.entities.Paciente;
+import upe.br.consultas.infra.enums.SexoEnum;
+
+import java.util.ArrayList;
+
+public record PacienteDTO(
+        Integer id,
+        String nome,
+        String cpf,
+        String email,
+        String telefone,
+        ArrayList<Byte> historico,
+        SexoEnum sexo,
+        ArrayList<String> restricoes
+) {
+    public static PacienteDTO pacienteToDTO (Paciente paciente) {
+        return new PacienteDTO(
+                paciente.getId(),
+                paciente.getNome(),
+                paciente.getCpf(),
+                paciente.getEmail(),
+                paciente.getTelefone(),
+                paciente.getHistorico(),
+                paciente.getSexo(),
+                paciente.getRestricoes()
+        );
+    }
 }
