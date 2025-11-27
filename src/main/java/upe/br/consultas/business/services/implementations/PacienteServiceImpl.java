34af37d76547ce4f2c8785391758651d74015b63
Murilo Lucena <muriloglucenacanal@gmail.com>
2025-11-27 16:46:54 -0300

    feat: adição da estrutura do ServiceImplement

--- src/main/java/upe/br/consultas/business/services/implementations/PacienteServiceImpl.java
+++ src/main/java/upe/br/consultas/business/services/implementations/PacienteServiceImpl.java
@@ -1,4 +1,53 @@
 package upe.br.consultas.business.services.implementations;
 
-public class PacienteServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import upe.br.consultas.business.services.interfaces.PacienteService;
+import upe.br.consultas.controller.DTO.paciente.PacienteDTO;
+import upe.br.consultas.infra.repositories.PacienteRepository;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class PacienteServiceImpl implements PacienteService {
+
+    @Autowired
+    PacienteRepository pacienteRepository;
+
+    @Override
+    public PacienteDTO cadastrarPaciente(PacienteDTO pacienteDTO) {
+        return null;
+    }
+
+    @Override
+    public PacienteDTO atualizarPaciente(PacienteDTO pacienteDTO) {
+        return null;
+    }
+
+    @Override
+    public void excluirPaciente(PacienteDTO pacienteDTO) {
+
+    }
+
+    @Override
+    public List<PacienteDTO> listarPacientes() {
+        return List.of();
+    }
+
+    @Override
+    public PacienteDTO buscarPacientePorId(Integer id) {
+        return null;
+    }
+
+    @Override
+    public PacienteDTO buscarPacientePorNome(String nome) {
+        return null;
+    }
+
+    @Override
+    public PacienteDTO buscarPacientePorCpf(String cpf) {
+        return null;
+    }
 }
