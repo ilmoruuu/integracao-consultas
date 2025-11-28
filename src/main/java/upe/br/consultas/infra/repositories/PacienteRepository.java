30a4e7b5583708c7f6a6f58b38c8793d9bf88807
Murilo Lucena <muriloglucenacanal@gmail.com>
2025-11-28 11:18:02 -0300

    feat: criação dos métodos do PacienteRepository

--- src/main/java/upe/br/consultas/infra/repositories/PacienteRepository.java
+++ src/main/java/upe/br/consultas/infra/repositories/PacienteRepository.java
@@ -4,7 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import upe.br.consultas.infra.entities.Paciente;
 
-@Repository
-public interface PacienteRepository extends JpaRepository<Paciente, String> {
+import java.util.Optional;
 
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Integer> {
+    @Override
+    Optional<Paciente> findById(Integer id);
+    Optional<Paciente> findByNome(String nome);
+    Optional<Paciente> findByCpf(String cpf);
+    Optional<Paciente> findByEmail(String email);
 }
