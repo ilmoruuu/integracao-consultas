5ca266190af931521e8abf468ba9d86ebd1f1a17
holyvieri <beatriz.oliveirac@upe.br>
2025-12-02 19:35:26 -0300

    fix: recepcionista name

--- src/main/java/upe/br/consultas/infra/repositories/RecepcionistaRepository.java
+++ src/main/java/upe/br/consultas/infra/repositories/RecepcionistaRepository.java
@@ -2,14 +2,14 @@ package upe.br.consultas.infra.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import upe.br.consultas.infra.entities.Recepicionista;
+import upe.br.consultas.infra.entities.Recepcionista;
 
 import java.util.Optional;
 
 @Repository
-public interface RecepcionistaRepository extends JpaRepository<Recepicionista, Integer> {
+public interface RecepcionistaRepository extends JpaRepository<Recepcionista, Integer> {
 
-    Optional<Recepicionista> findRecepicionistaById(Integer id);
-    Optional<Recepicionista> findRecepicionistaByNome(String nome);
+    Optional<Recepcionista> findRecepcionistaById(Integer id);
+    Optional<Recepcionista> findRecepcionistaByNome(String nome);
 
 }
