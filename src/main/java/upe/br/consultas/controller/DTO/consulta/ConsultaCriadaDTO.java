60130a6e079ec35cd9682ca75c2e42376ac56d3c
Murilo Lucena <muriloglucenacanal@gmail.com>
2025-12-04 00:51:14 -0300

    feat: atualização do DTO da Consulta criada com materiais e valores

--- src/main/java/upe/br/consultas/controller/DTO/consulta/ConsultaCriadaDTO.java
+++ src/main/java/upe/br/consultas/controller/DTO/consulta/ConsultaCriadaDTO.java
@@ -1,11 +1,14 @@
 package upe.br.consultas.controller.DTO.consulta;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public record ConsultaCriadaDTO(
         LocalDate data,
         String descricao,
         Integer medicoId,
         Integer pacienteId,
-        Integer recepcionistaId 
+        Integer recepcionistaId,
+        List<String> materiaisRequisitados,
+        double valorConsulta
 ) {}
